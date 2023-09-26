d0181a4c3da1040a65c92d6a5ee93289cc270664
hello <tom@live.se>
2023-09-27 00:34:56 +0200

    d

--- bluez/shared/u_device.c
+++ bluez/shared/u_device.c
@@ -6,6 +6,8 @@
 #include <sys/socket.h>
 #include <errno.h>
 
+#include <netinet/in.h>
+
 #include "b_device.h"
 #include "utility.h"
 
@@ -17,51 +19,60 @@
 int u_init_server()
 {
 
-    int server_socket, client_socket;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
 
-    // Create socket
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_socket == -1)
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("Socket creation error");
-        exit(1);
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Configure server address
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    // Bind socket to the server address
-    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1)
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET,
+                   SO_REUSEADDR | 15, &opt,
+                   sizeof(opt)))
     {
-        perror("Bind error");
-        close(server_socket);
-        exit(1);
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
     }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
 
-    // Listen for incoming connections
-    if (listen(server_socket, 5) == -1)
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address)) < 0)
     {
-        perror("Listen error");
-        close(server_socket);
-        exit(1);
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
-
-    printf("Server is listening on port %d...\n", PORT);
-
-    // Accept incoming connection
-    client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
-    if (client_socket == -1)
+    if (listen(server_fd, 3) < 0)
     {
-        perror("Accept error");
-        close(server_socket);
-        exit(1);
+        perror("listen");
+        exit(EXIT_FAILURE);
     }
-    return server_socket;
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t *)&addrlen)) < 0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+    send(new_socket, hello, strlen(hello), 0);
+    printf("Hello message sent\n");
+
+    // closing the connected socket
+    close(new_socket);
+    // closing the listening socket
+    shutdown(server_fd, SHUT_RDWR);
+    return 0;
 }
 
 int u_device_connect(char *ip_dress)

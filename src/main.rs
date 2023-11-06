03d676f3550b8da4a777c7ac38b11e888be0e409
Your Name <you@example.com>
2023-11-06 12:32:09 +0100

    yep

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,53 @@ use robotproject::{
 
 fn main() {
     unsafe {
-        let s = String::from("HalloWelt!");
-        let cs = CString::new(s).unwrap();
-        let cv: Vec<u8> = cs.into_bytes_with_nul();
-        let mut tmp: Vec<i8> = cv.into_iter().map(|c| c as i8).collect::<_>(); // line 7
-        let _cptr: *mut i8 = tmp.as_mut_ptr();
+        // let s = String::from("HalloWelt!");
+        // let cs = CString::new(s).unwrap();
+        // let cv: Vec<u8> = cs.into_bytes_with_nul();
+        // let mut tmp: Vec<i8> = cv.into_iter().map(|c| c as i8).collect::<_>(); // line 7
+        // let _cptr: *mut i8 = tmp.as_mut_ptr();
 
-        cbinding::bindings::takee_pic(_cptr);
+        // cbinding::bindings::takee_pic(_cptr);
 
-        //  let fd = cbinding::serial_open();
+        let fd = cbinding::serial_open();
 
-        //  let pos = protocol::GetPoseR::send_immediate_command(fd).unwrap();
+        let pos = protocol::GetPoseR::send_immediate_command(fd).unwrap();
 
-        // protocol::ptp::Cmd::send_immediate_command(
-        //     fd,
-        //     &protocol::ptp::PTPMode::MovlXYZ,
-        //     &FloatCustom::new(200.0),
-        //     &FloatCustom::new(0.0),
-        //     &FloatCustom::new(0.0),
-        //     &pos.r,
-        // );
+        // protocol::SuctionCup::send_immediate_command(fd, &1, &1);
+
+        // thread::sleep(Duration::from_millis(2000));
         // protocol::SuctionCup::send_immediate_command(fd, &0, &0);
 
         // protocol::EMotor::send_immediate_command(fd, &0, &1, &IntCustom::new(10000));
-        // // [0x27, 0x10, 0x00, 0x00]
 
-        // thread::sleep(Duration::from_millis(2000));
+        // thread::sleep(Duration::from_millis(3000));
         // protocol::EMotor::send_immediate_command(fd, &0, &1, &IntCustom::new(0));
 
-        //  protocol::GetPoseR::send_immediate_command(fd);
+        queue::StopExec::send_immediate_command(fd);
+        queue::ClearExec::send_immediate_command(fd);
+        let mut last_index = 0;
+        let mut f = FloatCustom::new(0.0);
+        let mut y_add = 10.0;
+
+        for i in 0..5 {
+            y_add += 20.0;
+            last_index = protocol::ptp::Cmd::send_queue_command(
+                fd,
+                &protocol::ptp::PTPMode::MovlXYZ,
+                &FloatCustom::new(175.0),
+                &FloatCustom::new(0.0),
+                &FloatCustom::new(0.0),
+                &pos.r,
+            )
+            .unwrap();
+        }
+
+        queue::StartExec::send_immediate_command(fd);
+
+        while protocol::queue::CurrentIndex::send_get_command(fd)
+            .unwrap()
+            .current_index
+            >= last_index
+        {}
     }
 }
